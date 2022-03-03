1f7230a69c048705d2b212f082fd9c063bf56300
Nico Mexis <nico.mexis@kabelmail.de>
2022-03-03 15:30:39 +0100

    Fix OffsetDateTimes for gson

--- src/main/java/de/femtopedia/powasysbackend/util/Util.java
+++ src/main/java/de/femtopedia/powasysbackend/util/Util.java
@@ -2,13 +2,24 @@ package de.femtopedia.powasysbackend.util;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializer;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 public final class Util {
 
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ssXXXXX");
+
     public static final Gson GSON = new GsonBuilder()
             .disableHtmlEscaping()
             .setPrettyPrinting()
+            .registerTypeAdapter(OffsetDateTime.class,
+                    (JsonDeserializer<OffsetDateTime>) (json, type, ctx) -> OffsetDateTime.parse(json.getAsJsonPrimitive().getAsString(), FORMATTER))
+            .registerTypeAdapter(OffsetDateTime.class,
+                    (JsonSerializer<OffsetDateTime>) (obj, type, ctx) -> new JsonPrimitive(FORMATTER.format(obj)))
             .create();
 
     public static Optional<Double> parseDouble(String toParse) {
